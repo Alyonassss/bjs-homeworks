5a6208772f9d7942b32ebc9afad6da38758196a9
Alyonassss <aln_sycheva@mail.ru>
2021-04-19 20:26:03 +0300

    first commit extended-syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,64 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    "use strict";
+ 
+    let d;
+    d = (b * b) - (4 * (a * c));
+
+    if (d > 0) {
+        let x1 = (((-b + (Math.sqrt(d)))) / (2 * a)),
+            x2 = (((-b - (Math.sqrt(d)))) / (2 * a));
+        
+        return [x1, x2];
+
+    } else if (d === 0) {
+        let x3 = (((-b + (Math.sqrt(d)))) / (2 * a));
+        return [x3];
+    
+    } else if (d < 0) {
+        return [];
+    }
+
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+
+    let sumResult = 0;
+    let averageMark = '';
+    const numberMarks = 5;
+    
+    if (marks.length === 0) {
+        return marks.length;
+    }
+
+    for (let i = 0; i < marks.length; i++) {
+      sumResult = sumResult + marks[i];
+      if (marks.length > numberMarks) {
+          marks.splice(numberMarks);
+      } 
+    }
+
+    averageMark = sumResult / marks.length;
+    console.log(averageMark);
+    
+    return averageMark;
 }
 
+
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+
+   let currentDate  = new Date();
+   let personAge = currentDate.getFullYear() - dateOfBirthday.getFullYear();
+   
+   let result = "";
+
+   if (personAge >= 18) {
+     result = `Не желаете ли олд-фэшн, ${name}?`;
+     console.log(result);
+     return result;
+   } else {
+     result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
+     console.log(result);
+     return result;
+   }
+    
 }
