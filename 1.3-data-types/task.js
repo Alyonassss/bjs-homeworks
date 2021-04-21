494a8dd6c45668f276eebfdaf4db7a5627a8bd1f
Alyonassss <aln_sycheva@mail.ru>
2021-04-21 21:47:38 +0300

    first commit variables

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,45 @@
+"use strict"
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+  if (isNaN(percent) || percent <= 0) {
+    return `Параметр Процентная ставка содержит неправильное значение ${percent}.`;
+  }
+
+  if (isNaN(contribution) || contribution < 0 || contribution > amount) {
+    return `Параметр Начальный взнос содержит неправильное значение ${contribution}.`;
+  }
+
+  if (isNaN(amount) || amount <= 0) {
+    return `Параметр Общая стоимость содержит неправильное значение ${amount}.`;
+  }
+
+  if (isNaN(Date.parse(date))) {
+    return `Параметр Срок ипотеки содержит неправильное значение ${date}.`;
+  }
+
+  let refundAmount = amount - contribution;
+  let startDate = new Date;
+  let numberOfMonths = Math.round((date - startDate) / 2628000000);
+
+  if (numberOfMonths < 1) {
+    return `Параметр Срок ипотеки содержит неправильное значение ${numberOfMonths}.`;
+  }
+
+  let monthlyInterestRate = percent / 100 / 12;
+  let monthlyPayment = refundAmount * (monthlyInterestRate + monthlyInterestRate / (((1 + monthlyInterestRate) ** numberOfMonths) - 1));
+  let totalAmount = +(monthlyPayment * numberOfMonths).toFixed(2);
+  console.log(totalAmount);
+  return totalAmount;
+
 }
 
+
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+
+    if ((!name) || (name == typeof (undefined)) || (name === 'null') || (name == `''`)) {
+        name = 'Аноним';
+    }
+
+    return `Привет, мир! Меня зовут ${name}`;
+
 }
